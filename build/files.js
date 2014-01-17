a95cbd3eea2b29553f3757b71bedcff2bd8489d8
Rod Vagg <rod@vagg.org>
2014-01-17 21:31:02 +1100

    added back Buffer as a require()

--- build/files.js
+++ build/files.js
@@ -7,6 +7,13 @@
  */
 
 module.exports['string_decoder.js'] = [
+
+    // pull in Bufer as a require
+    [
+        /^(\/\/ USE OR OTHER DEALINGS IN THE SOFTWARE\.)/m
+      , '$1\n\nvar Buffer = require(\'buffer\').Buffer;'
+    ]
+
 ]
 
 module.exports['string_decoder.js'].out = 'index.js'
