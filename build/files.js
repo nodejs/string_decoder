f26b3346d7c3c9d49bf7977af21867bf87d63e56
Rod Vagg <rod@vagg.org>
2014-01-17 21:27:37 +1100

    added and adapted scraper from readable-stream

---
+++ build/files.js
@@ -0,0 +1,12 @@
+/* This file lists the files to be fetched from the node repo
+ * in the /lib/ directory which will be placed in the ../lib/
+ * directory after having each of the "replacements" in the
+ * array for that file applied to it. The replacements are
+ * simply the arguments to String#replace, so they can be
+ * strings, regexes, functions.
+ */
+
+module.exports['string_decoder.js'] = [
+]
+
+module.exports['string_decoder.js'].out = 'index.js'
